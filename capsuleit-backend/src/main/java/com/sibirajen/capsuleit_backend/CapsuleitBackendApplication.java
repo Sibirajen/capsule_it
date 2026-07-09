0a79183a523b899c12383fdfb3f9c1fc43cc65cc
Sibirajen <Sibirajen16@gmail.com>
2026-07-09 17:52:05 +0530

    backend: Initial commit

---
+++ capsuleit-backend/src/main/java/com/sibirajen/capsuleit_backend/CapsuleitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sibirajen.capsuleit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsuleitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapsuleitBackendApplication.class, args);
+	}
+
+}
