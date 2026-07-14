33e84cd810f1bd05cc911b7a6f15dd71e5251617
Sibirajen <Sibirajen16@gmail.com>
2026-07-14 22:37:47 +0530

    frontend: Initial commit

---
+++ capsuleit-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-950">
+      <h1 className="text-5xl font-bold text-white">
+        CapsuleIt 🚀
+      </h1>
+    </div>
+  );
+}
