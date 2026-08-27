a70c3e0cd55eeefbe7c7e12dc39b009df853fafa
Sibirajen <Sibirajen16@gmail.com>
2026-08-27 11:09:58 +0530

    backend: added capsule controller with basic curd functionality

---
+++ capsuleit-backend/src/main/java/com/sibirajen/capsuleit_backend/capsule/CapsuleController.java
@@ -0,0 +1,35 @@
+package com.sibirajen.capsuleit_backend.capsule;
+
+import com.sibirajen.capsuleit_backend.capsule.dto.CapsuleRequest;
+import com.sibirajen.capsuleit_backend.capsule.dto.CapsuleResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/capsule")
+public class CapsuleController {
+    private final CapsuleService capsuleService;
+
+    @Autowired
+    public  CapsuleController(CapsuleService capsuleService) {
+        this.capsuleService = capsuleService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<CapsuleResponse> createCapsule(@RequestBody CapsuleRequest capsuleRequest) {
+        return ResponseEntity.ok(capsuleService.createCapsule(capsuleRequest));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CapsuleResponse>> getAllCapsules() {
+        return ResponseEntity.ok(capsuleService.getAllCapsules());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CapsuleResponse> getCapsule(@PathVariable Long id) {
+        return ResponseEntity.ok(capsuleService.getCapsule(id));
+    }
+}
