74ee0cf14608012e01f6d44a8fb2eee902ab38af
Sibirajen <Sibirajen16@gmail.com>
2026-09-01 18:11:11 +0530

    frontend: added color variables

--- capsuleit-frontend/src/App.tsx
+++ capsuleit-frontend/src/App.tsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { DashboardLayout } from "./components/layout/DashboardLayout";
+
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-950">
-      <h1 className="text-5xl font-bold text-white">
-        CapsuleIt 🚀
-      </h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<DashboardLayout />}>
+          <Route index element={<h1>Home Page</h1>} />
+          <Route path="capsules" element={<h1>Capsules</h1>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
